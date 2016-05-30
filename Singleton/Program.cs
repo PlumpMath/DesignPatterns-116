1eff67950da8ea2cd81759661b3510f4c508ac35
bzyzhang <longfeiah@gmail.com>
2016-05-30 22:20:15 +0800

    Singleton
    单例模式

---
+++ Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("两个对象是相同的实例。");
+            }
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Singleton
+{
+    /// <summary> 
+    /// 作者：bzyzhang
+    /// 时间：2016/5/30 22:10:39 
+    /// 博客地址：http://www.cnblogs.com/bzyzhang/
+    /// Singleton说明：本代码版权归bzyzhang所有，使用时必须带上bzyzhang博客地址 
+    /// </summary> 
+    public class Singleton
+    {
+        ///实现一
+        //private static Singleton instance;
+
+        //private Singleton()
+        //{
+        //}
+
+        //public static Singleton GetInstance()
+        //{
+        //    if (instance == null)
+        //    {
+        //        instance = new Singleton();
+        //    }
+        //    return instance;
+        //}
+
+        /// <summary>
+        /// 实现二，多线程时的单例
+        /// </summary>
+        //private static Singleton instance;
+
+        //private static readonly object syncRoot = new object();
+
+        //private Singleton() { }
+
+        //public static Singleton GetInstance()
+        //{
+        //    lock (syncRoot)
+        //    {
+        //        if (instance == null)
+        //        {
+        //            instance = new Singleton();
+        //        }
+        //    }
+        //    return instance;
+        //}
+
+        /// <summary>
+        /// 实现三，双重锁定
+        /// </summary>
+        //private static Singleton instance;
+
+        //private static readonly object syncRoot = new object();
+
+        //private Singleton() { }
+
+        //public static Singleton GetInstance()
+        //{
+        //    if (instance == null)
+        //    {
+        //        lock (syncRoot)
+        //        {
+        //            if (instance == null)
+        //            {
+        //                instance = new Singleton();
+        //            }
+        //        }
+        //    }
+        //    return instance;
+        //}
+
+        /// <summary>
+        /// 实现四，静态初始化
+        /// </summary>
+        private static readonly Singleton instance = new Singleton();
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            return instance;
+        }
+    }
+}
